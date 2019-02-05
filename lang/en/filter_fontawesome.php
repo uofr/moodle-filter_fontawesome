894b45a1076c51ea49d10b06874b7b2108b1672d
Adrian Perez <p.adrian@gmx.ch>
2019-02-05 17:51:36 +0100

    Add pluginname (Fixes #4)

--- lang/en/filter_fontawesome.php
+++ lang/en/filter_fontawesome.php
@@ -24,6 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$string['pluginname'] = 'FontAwesome icons';
 $string['filtername'] = 'FontAwesome icons';
 $string['settingformats'] = 'Apply to formats';
 $string['settingformats_desc'] = 'The filter will be applied only if the original text was inserted in one of the selected formats.';
